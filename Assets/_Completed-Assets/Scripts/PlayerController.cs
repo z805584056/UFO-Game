6006e7b8ae850cb95d021046a0cbded659c62784
z805584056 <z568559637@gmail.com>
2018-10-03 01:39:19 -0400

    Complete

--- Assets/_Completed-Assets/Scripts/PlayerController.cs
+++ Assets/_Completed-Assets/Scripts/PlayerController.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     public float speed;
+    public Text countText;
+    public Text winText;
 
     private Rigidbody2D rb2d;
+    private int count;
 
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        winText.text = "";
+        SetCountText();
     }
 
     private void FixedUpdate()
@@ -19,5 +26,25 @@ public class PlayerController : MonoBehaviour {
         float moveVertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
         rb2d.AddForce (movement*speed);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        
+            if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >=12)
+        {
+            winText.text = "You win!";
+        }
     }
 }
